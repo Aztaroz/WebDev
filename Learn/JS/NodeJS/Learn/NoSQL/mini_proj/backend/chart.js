0b562537199a6443206f4b8f2034fea083f616be
Aztaroz <flight1459@gmail.com>
2022-12-25 17:54:35 +0700

    update mini project nosql

--- Learn/JS/NodeJS/Learn/NoSQL/mini_proj/backend/chart.js
+++ Learn/JS/NodeJS/Learn/NoSQL/mini_proj/backend/chart.js
@@ -1,10 +1,11 @@
-// google.charts.load("current", { 'packages': ["corechart"] });
+google.charts.load("current", { 'packages': ["corechart"] });
 google.charts.load('current', { 'packages': ['bar'] });
+google.charts.load('current', { 'packages': ['line'] });
 google.charts.setOnLoadCallback(loadSort);
 
 function loadSort() {
     const xhttp = new XMLHttpRequest();
-    var All = '<option selected>All</option>'
+    // var All = '<option selected>All</option>'
     var Year_option = ''
     var TOD_option = ''
     var AG_option = ''
@@ -12,31 +13,31 @@ function loadSort() {
 
     var year = []
     var TOD = []
-    var AG = []
+    // var AG = []
     var Sex = []
 
     xhttp.open("GET", "http://localhost:4596/sleepdata");
     xhttp.send();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            console.log("connected");
+            console.log("Call loadSort");
             const objects = JSON.parse(this.responseText);
 
             for (let object of objects) {
                 year.push(parseInt(object["Year"]))
                 TOD.push(object["Type of Days"])
-                AG.push(object["Age Group"])
+                // AG.push(object["Age Group"])
                 Sex.push(object["Sex"])
             }
 
             var uniqueYear_Arr = new Set(year)
             var uniqueTOD_Arr = new Set(TOD)
-            var uniqueAG_Arr = new Set(AG)
+            // var uniqueAG_Arr = new Set(AG)
             var uniqueSex_Arr = new Set(Sex)
 
             var year_Array = [...uniqueYear_Arr]
             var TOD_Array = [...uniqueTOD_Arr]
-            var AG_Array = [...uniqueAG_Arr]
+            // var AG_Array = [...uniqueAG_Arr]
             var Sex_Array = [...uniqueSex_Arr]
 
             AG_option += '<option selected>All</option>'
@@ -47,20 +48,17 @@ function loadSort() {
             for (var x of TOD_Array) {
                 TOD_option += `<option  value='${x}'>` + x + "</option>";
             }
-            for (var x of AG_Array) {
-                AG_option += `<option  value='${x}'>` + x + "</option>";
-            }
+            // for (var x of AG_Array) {
+            //     AG_option += `<option  value='${x}'>` + x + "</option>";
+            // }
             for (var x of Sex_Array) {
                 Sex_option += `<option  value='${x}'>` + x + "</option>";
             }
 
             document.getElementById("inputYear").innerHTML = Year_option;
             document.getElementById("inputTOD").innerHTML = TOD_option;
-            document.getElementById("inputAG").innerHTML = AG_option;
+            // document.getElementById("inputAG").innerHTML = AG_option;
             document.getElementById("inputSex").innerHTML = Sex_option;
-
-
-
 
             drawChart()
         }
@@ -68,14 +66,16 @@ function loadSort() {
 }
 
 function drawChart() {
+    var year = document.getElementById("inputYear").value
+    var tod = document.getElementById("inputTOD").value
+    var sex = document.getElementById("inputSex").value
     const xhttp = new XMLHttpRequest();
     xhttp.open("GET", "http://localhost:4596/sleepdata");
     xhttp.send();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             const objects = JSON.parse(this.responseText);
-            var count = 1
-            var sum = 0
+            console.log("Call drawChart");
             // 15 to 24 years
             // 25 to 34 years
             // 35 to 44 years
@@ -83,42 +83,90 @@ function drawChart() {
             // 55 to 64 years
             // 65 years and over
             for (object of objects) {
-                if (object['Year'] == "2003") {
-                    if (object['Type of Days'] == 'All days') {
-                        if (object['Sex'] == 'Both') {
-                            
-
+                if (object['Year'] == year) {
+                    if (object['Type of Days'] == tod) {
+                        if (object['Sex'] == sex) {
                             switch (object['Age Group']) {
                                 case "15 to 24 years":
-                                    console.log(object['Avg hrs per day sleeping'])
+                                    var a = parseFloat(object['Avg hrs per day sleeping'])
                                     break;
-                            
+                                case "25 to 34 years":
+                                    var b = parseFloat(object['Avg hrs per day sleeping'])
+                                    break;
+                                case "35 to 44 years":
+                                    var c = parseFloat(object['Avg hrs per day sleeping'])
+                                    break;
+                                case "45 to 54 years":
+                                    var d = parseFloat(object['Avg hrs per day sleeping'])
+                                    break;
+                                case "55 to 64 years":
+                                    var e = parseFloat(object['Avg hrs per day sleeping'])
+                                    break;
+                                case "65 years and over":
+                                    var f = parseFloat(object['Avg hrs per day sleeping'])
+                                    break;
                                 default:
+                                    console.log("Error");
                                     break;
                             }
+                            var data = google.visualization.arrayToDataTable([
+                                ["Year", "Average hours per day sleeping", { role: "style" }],
+                                ["15 to 24 years", a, "#9102F7"],
+                                ["25 to 34 years", b, "#9C4EDC"],
+                                ["35 to 44 years", c, "#7A2BBF"],
+                                ["45 to 54 years", d, "color: #5A199B"],
+                                ["55 to 64 years", e, "color: #3C0B6C"],
+                                ["65 years and over", f, "color: #240147"]
+                            ]);
+
+                            var view = new google.visualization.DataView(data);
+                            view.setColumns([0, 1,
+                                {
+                                    calc: "stringify",
+                                    sourceColumn: 1,
+                                    type: "string",
+                                    role: "annotation"
+                                },
+                                2]);
+
+                            var options = {
+                                title: `Sleep Data in year : ${year}`,
+                                width: 1300,
+                                height: 660,
+                                bar: { groupWidth: "95%" },
+                                legend: { position: "none" },
+                            }
+                            var chart = new google.visualization.BarChart(document.getElementById("barchart_values"));
+                            chart.draw(view, options);
+
                         }
                     }
                 }
             }
-            var avg = sum / count
-            console.log(avg);
+            // var avg = sum / count
+            // console.log(avg);
 
 
-            var data = google.visualization.arrayToDataTable([
-                ['Year', '15 to 24 years', '25 to 34 years', '35 to 44 years','45 to 54 years','55 to 64 years','65 years and over'],
-                ['2003', 1000, 400, 200, 300, 700, 900],
-            ]);
 
-            var options = {
-                chart: {
-                    title: 'Company Performance',
-                    subtitle: 'Sales, Expenses, and Profit: 2014-2017',
-                }
-            };
+            // var data = google.visualization.arrayToDataTable([
+            //     ['Year', '15 to 24 years', '25 to 34 years', '35 to 44 years', '45 to 54 years', '55 to 64 years', '65 years and over'],
+            //     ['2003', a, b, c, d, e, f],
+            // ]);
 
-            var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+            // var options = {
+            //     chart: {
+            //         title: 'Company Performance',
+            //         subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+            //     }
+            // };
 
-            chart.draw(data, google.charts.Bar.convertOptions(options));
+            // var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+
+            // chart.draw(data, google.charts.Bar.convertOptions(options));
+
+
+
+
 
             // var data = google.visualization.arrayToDataTable([
             //     ['Year', 'Sales', 'Expenses', 'Profit'],
@@ -148,33 +196,17 @@ function drawChart() {
     }
 }
 
-function loadGraph() {
-    var year = document.getElementById("inputYear").value
-    var tod = document.getElementById("inputTOD").value
-    var ag = document.getElementById("inputAG").value
-    var sex = document.getElementById("inputSex").value
+// function loadQueryChart() {
 
-    console.log(year)
-    const xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "http://localhost:4596/sleepdata");
-    xhttp.send();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            console.log("connected");
-            const objects = JSON.parse(this.responseText);
-
-            for (let object of objects) {
-                if (object["Year"] == year) {
-
-                    if (object["Type of Days"] == tod) {
-                        if (object["Age Group"] == ag) {
-                            if (object["Sex"] == sex) {
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
+//     const xhttp = new XMLHttpRequest();
+//     xhttp.open("GET", "http://localhost:4596/sleepdata");
+//     xhttp.send();
+//     xhttp.onreadystatechange = function () {
+//         if (this.readyState == 4 && this.status == 200) {
+//             console.log("Call loadQueryChart");
+//             const objects = JSON.parse(this.responseText);
+//             console.log("Hello From Query Chart");
+//         }
+//     }
+// }
 
