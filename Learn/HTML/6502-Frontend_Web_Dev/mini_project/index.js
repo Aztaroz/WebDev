64b6ea249a826a47400e44d201159c8ac6e38b1a
Aztaroz <flight1459@gmail.com>
2023-01-05 20:18:35 +0700

    update 'PUT' function to index.js

--- Learn/HTML/6502-Frontend_Web_Dev/mini_project/index.js
+++ Learn/HTML/6502-Frontend_Web_Dev/mini_project/index.js
@@ -57,18 +57,18 @@ function createData() {
 function readData() {
     console.log("function createData() is called");
     var html = '<div class="row">'
-
     fetch(url + '/user')
         .then(response => response.json())
         .then(data => {
             for (const x of data) {
-                console.log(x.id);
+                // console.log(x.id);
                 html += `<div class="card" style="width: 18rem;">
                 <img src="${x.img}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${x.id}</h5>
                     <p class="card-text">รายการจองของ ${x.user} ${x.sname}</p>
                     <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <button class="btn btn-warning" onclick="editData(${x.id})">Test Edit</button>
                 </div>
             </div>`
             }
@@ -80,7 +80,7 @@ function readData() {
 function deleteData() {
     var deletetb = document.getElementById('deletetb').value
     console.log(deletetb);
-    
+
     fetch(url + `/user/${deletetb}`, {
         method: 'DELETE',
         headers: {
@@ -93,4 +93,56 @@ function deleteData() {
         .then(response => response.json())
         .then(data => console.log(data))
         .catch(error => console.log(error))
+}
+
+
+function editData(id) {
+    var name =''
+    var sname =''
+    var img =''
+    
+    // console.log(id);
+    fetch(url + `/user/${id}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+                    Swal.fire({
+                        title: `Edit username ${data['user']}`,
+                        html:
+                            `<input id="swal-input1" class="swal2-input" disabled value="${data['id']}">
+                            <input id="swal-input2" class="swal2-input" value="${data['user']}">
+                            <input id="swal-input3" class="swal2-input" value="${data['sname']}">
+                            <input id="swal-input4" class="swal2-input" value="${data['img']}">`,
+
+                        focusConfirm: false,
+                        preConfirm: () => {
+                            return [
+                                name = document.getElementById('swal-input2').value,
+                                sname = document.getElementById('swal-input3').value,
+                                img = document.getElementById('swal-input4').value,
+                                fetch(url + `/user/${id}`, {
+                                    method: 'PUT',
+                                    headers: {
+                                        'Content-Type': 'application/json'
+                                    },
+                                    body: JSON.stringify({
+                                        "id": id,
+                                        "user": name,
+                                        "sname": sname,
+                                        "img": img
+                                    })
+                                }).then(response => response.json())
+                                    .then(data => console.log(data))
+                                    .catch(error => console.log(error))
+                            ]
+                        }
+                    })
+
+
+        })
+
+
+    // if (formValues) {
+    //     Swal.fire(JSON.stringify(formValues))
+    // }
 }
